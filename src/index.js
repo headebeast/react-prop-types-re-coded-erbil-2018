1e5a76cf053fff7c614ce93531cc1c1512e5258b
Hedi Sabah <headesabah@gmail.com>
2018-09-13 06:19:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class HiReCoded extends React.Component {
+  render() {
+    return (
+      <h1>{this.props.name}</h1>
+    );
+  }
+}
+
+HiReCoded.propTypes = {
+  name: PropTypes.string
+};// Code Goes Here
